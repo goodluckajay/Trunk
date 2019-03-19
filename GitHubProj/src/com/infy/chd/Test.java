de828dc14dd5761380dc7ca7274b06f7d388bbc5
Ajay.Dhiman <Ajay.Dhiman@CHDSEZ148138L.ad.infosys.com>
2019-03-19 11:29:08 +0530

    ok

--- GitHubProj/src/com/infy/chd/Test.java
+++ GitHubProj/src/com/infy/chd/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello Git Hub !!");
 		System.out.println("Automatically saved in Local");
+		System.out.println("Going to pull exising code from github");
 
 	}
 
