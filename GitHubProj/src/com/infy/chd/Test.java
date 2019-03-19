36ba1e355ee553f92870a954d3c1cf29603d75de
Ajay.Dhiman <Ajay.Dhiman@CHDSEZ148138L.ad.infosys.com>
2019-03-19 11:21:18 +0530

    ok

--- GitHubProj/src/com/infy/chd/Test.java
+++ GitHubProj/src/com/infy/chd/Test.java
@@ -3,7 +3,8 @@ package com.infy.chd;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello Git Hub !!");
+		System.out.println("Automatically saved in Local");
 
 	}
 
